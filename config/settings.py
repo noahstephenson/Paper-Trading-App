41998ad1d9993115696584bb2e153fb01074b74d
Noah Stephenson <noahhockey72@gmail.com>
2026-04-22 13:25:07 -0400

    Add custom domain to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-84&)hq$6o09tyyeaa&ms!
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com', 'trading.noahjstephenson.com']
 
 
 INSTALLED_APPS = [
